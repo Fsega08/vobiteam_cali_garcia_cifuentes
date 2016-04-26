9eeec3a5cce75b01b0ba2fd5f773777589b18a31
Freddy Sebastián García Victoria <fsega08@gmail.com>
2016-04-26 08:09:59 -0500

    menu por roles listo

--- vobiTeam/src/main/java/com/vobi/team/presentation/backingBeans/MenuModelView.java
+++ vobiTeam/src/main/java/com/vobi/team/presentation/backingBeans/MenuModelView.java
@@ -14,7 +14,6 @@ import org.primefaces.model.menu.DefaultSubMenu;
 import org.primefaces.model.menu.MenuModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import com.vobi.team.modelo.VtUsuario;
@@ -30,61 +29,108 @@ public class MenuModelView {
 
 	@ManagedProperty(value="#{BusinessDelegatorView}")
 	private IBusinessDelegatorView businessDelegatorView;
-	
-	private MenuModel menuModel;
-	private DefaultSubMenu subMenu;
-	private DefaultMenuItem item;
 
+	private MenuModel menuModel;
+	
 	private String usuarioActual=SecurityContextHolder.getContext().getAuthentication().getName();
 
 	@PostConstruct
-	public void init() throws Exception{
-		
-		VtUsuario vtUsuario = businessDelegatorView.findUsuarioByLogin(usuarioActual);
-		List<VtUsuarioRol> vtUsuarioRoles = businessDelegatorView.findUsuarioRolbyUsuario(vtUsuario);
-		int permisos = businessDelegatorView.getVtRol().size();
-			
-		for (VtUsuarioRol vtUsuarioRol : vtUsuarioRoles) {
-			if (vtUsuarioRol.getVtRol().getRolCodigo() == 3L) {
-				if (permisos > vtUsuarioRol.getVtRol().getRolCodigo()) {
-					permisos = 3;
-				}
-			}else if (vtUsuarioRol.getVtRol().getRolCodigo() == 2L) {
-				if (permisos > vtUsuarioRol.getVtRol().getRolCodigo()) {
-					permisos = 2;
-				}
-			}else if (vtUsuarioRol.getVtRol().getRolCodigo() == 1L) {
-				if (permisos > vtUsuarioRol.getVtRol().getRolCodigo()) {
-					permisos = 1;
+	public void init() {
+		try {
+			menuModel = new DefaultMenuModel();
+			VtUsuario vtUsuario = businessDelegatorView.findUsuarioByLogin(usuarioActual);
+			List<VtUsuarioRol> vtUsuarioRoles = businessDelegatorView.findUsuarioRolbyUsuario(vtUsuario);
+			int permisos = businessDelegatorView.getVtRol().size();
+
+			for (VtUsuarioRol vtUsuarioRol : vtUsuarioRoles) {
+				if (vtUsuarioRol.getVtRol().getRolCodigo() == 3L) {
+					if (permisos > vtUsuarioRol.getVtRol().getRolCodigo()) {
+						permisos = 3;
+					}
+				}else if (vtUsuarioRol.getVtRol().getRolCodigo() == 2L) {
+					if (permisos > vtUsuarioRol.getVtRol().getRolCodigo()) {
+						permisos = 2;
+					}
+				}else if (vtUsuarioRol.getVtRol().getRolCodigo() == 1L) {
+					if (permisos > vtUsuarioRol.getVtRol().getRolCodigo()) {
+						permisos = 1;
+					}
 				}
 			}
+
+			if (permisos == 1) {
+				menuAdministrador();
+			}else if (permisos == 2) {
+				menuDesarrollador();
+			}else if (permisos ==3) {
+				menuCliente();
+			}
+		} catch (Exception e) {
+			log.error(e.getMessage());
 		}
 		
-		if (permisos == 1) {
-			menuAdministrador();
-		}else if (permisos == 2) {
-			menuDesarrollador();
-		}else if (permisos ==3) {
-			menuCliente();
-		}
-		
+
 	}
+	
+	public IBusinessDelegatorView getBusinessDelegatorView() {
+		return businessDelegatorView;
+	}
+
+
+
+	public void setBusinessDelegatorView(IBusinessDelegatorView businessDelegatorView) {
+		this.businessDelegatorView = businessDelegatorView;
+	}
+
+
+
+	public MenuModel getMenuModel() {
+		return menuModel;
+	}
+
+
+
+	public void setMenuModel(MenuModel menuModel) {
+		this.menuModel = menuModel;
+	}
+
+
 
 	private void menuCliente() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void menuDesarrollador() {
-		// TODO Auto-generated method stub
+		DefaultMenuItem menuDashboard = new DefaultMenuItem("Dashboard");
+		menuDashboard.setOutcome("dashboard.xhtml");
+		menuDashboard.setIcon("icon-home");
+		menuDashboard.setId("sm_Dashboard");
+		menuModel.addElement(menuDashboard);
 		
+		DefaultSubMenu orientacion = new DefaultSubMenu("Orientación del menu");		
+		
+		DefaultMenuItem derechaItem = new DefaultMenuItem("Derecha");
+		derechaItem.setIcon("icon-align-right");
+		derechaItem.setId("sm_rtl");
+		derechaItem.setOnclick("$('body').addClass('ui-sentinel-rtl');return false;");
+		
+		DefaultMenuItem izquierdaItem = new DefaultMenuItem("Izquierda");
+		izquierdaItem.setIcon("icon-align-left");
+		izquierdaItem.setId("sm_ltr");
+		izquierdaItem.setOnclick("$('body').removeClass('ui-sentinel-rtl');return false;");
+		
+		orientacion.setId("sm_orientation");
+		orientacion.setIcon("icon-align-right");
+		orientacion.addElement(derechaItem);
+		orientacion.addElement(izquierdaItem);
+		menuModel.addElement(orientacion);
+		
+
 	}
 
 	private void menuAdministrador() {
-		
-		
-		
-		
+
 		DefaultSubMenu empresaSubmenu = new DefaultSubMenu("Empresa");		
 		DefaultMenuItem empresaItem = new DefaultMenuItem("Lista de Empresas");
 
@@ -97,96 +143,83 @@ public class MenuModelView {
 		empresaSubmenu.setIcon("icon-home");
 		empresaSubmenu.addElement(empresaItem);
 		menuModel.addElement(empresaSubmenu);
-		
-		
-		/*
-		<p:submenu id="sm_Usuario" label="Usuario" icon="icon-male">
-			<p:menuitem id="sm_vtUsuario" value="Lista de Usuarios" icon="icon-list"
-				outcome="/XHTML/listaUsuarios" />
-		</p:submenu> */
-		
-		//Usuario
-		DefaultSubMenu usuarioSubmenu = new DefaultSubMenu("Usuarios");
-		DefaultMenuItem usuarioItem = new DefaultMenuItem("Usuarios");
 
-		usuarioItem.setOutcome("usuarioVista");
-		usuarioItem.setIcon("icon-user-add");
-		usuarioItem.setId("sm_usuarioVista");
+		//Usuario
+		DefaultSubMenu usuarioSubmenu = new DefaultSubMenu("Usuario");
+		DefaultMenuItem usuarioItem = new DefaultMenuItem("Lista de usuarios");
+
+		usuarioItem.setOutcome("/XHTML/listaUsuarios");
+		usuarioItem.setIcon("icon-list");
+		usuarioItem.setId("sm_vtUsuario");
 
 		usuarioSubmenu.setId("sm_Usuarios");
-		usuarioSubmenu.setIcon("icon-user-1");
+		usuarioSubmenu.setIcon("icon-male");
 		usuarioSubmenu.addElement(usuarioItem);
-
-
-		DefaultMenuItem rolItem = new DefaultMenuItem("Roles");
-		rolItem.setOutcome("rolVista");
-		rolItem.setIcon("icon-users");
-		rolItem.setId("sm_rolVista");
-
-		usuarioSubmenu.addElement(rolItem);
 		menuModel.addElement(usuarioSubmenu);
 
-		//Pila de producto
-		DefaultSubMenu pilaSubmenu = new DefaultSubMenu("Pila de producto");
-		DefaultMenuItem pilaItem = new DefaultMenuItem("Gestionar Pila");
 
-		pilaItem.setOutcome("pilaProductoVista");
-		pilaItem.setIcon("icon-align-justify");
-		pilaItem.setId("sm_pilaProductoVista");
+		//Propiedades del artefacto
 
-		pilaSubmenu.setId("sm_Pila_Producto");
-		pilaSubmenu.setIcon("icon-align-center");
-		pilaSubmenu.addElement(pilaItem);
-		menuModel.addElement(pilaSubmenu);
+		DefaultSubMenu propiedadesSM = new DefaultSubMenu("Otras Propiedades");
 
+		//Item Rol
+		DefaultMenuItem rolItem = new DefaultMenuItem("Gestion de Roles");
+		rolItem.setOutcome("/XHTML/listaRoles.xhtml");
+		rolItem.setIcon("icon-users-2");
+		rolItem.setId("sm_vtUsuario");
 
-		//Sprint
-		DefaultSubMenu sprintSubmenu = new DefaultSubMenu("Sprint");
-		DefaultMenuItem sprintItem = new DefaultMenuItem("Gestionar Sprint");
+		//Item Prioridad
+		DefaultMenuItem prioridadItem = new DefaultMenuItem("Gestión Prioridades del Artefacto");
+		prioridadItem.setOutcome("/XHTML/listaPrioridad.xhtml");
+		prioridadItem.setIcon("icon-menu-outline");
+		prioridadItem.setId("sm_vtPrioridad");
 
-		sprintItem.setOutcome("sprintVista");
-		sprintItem.setIcon("icon-th-list-1");
-		sprintItem.setId("sm_sprintVista");
+		//Item Estado
+		DefaultMenuItem estadoItem = new DefaultMenuItem("Gestión Estados del Artefacto");
+		estadoItem.setOutcome("/XHTML/listaEstado.xhtml");
+		estadoItem.setIcon("icon-archive");
+		estadoItem.setId("sm_vtEstado");
 
-		sprintSubmenu.setId("sm_Sprint");
-		sprintSubmenu.setIcon("icon-book");
-		sprintSubmenu.addElement(sprintItem);
-		menuModel.addElement(sprintSubmenu);
+		//Item Tipo
+		DefaultMenuItem tipoItem = new DefaultMenuItem("Gestión Tipos de Artefacto");
+		tipoItem.setOutcome("/XHTML/listaTipoArtefacto.xhtml");
+		tipoItem.setIcon("icon-clipboard");
+		tipoItem.setId("sm_vtTipo");
 
-
-		//Comportamiento artefacto
-		DefaultSubMenu cpArtefactoSubmenu = new DefaultSubMenu("Comportamiento Artefacto");
-
-		//Estado Artefacto
-		DefaultMenuItem estadoArtefactoItem = new DefaultMenuItem("Estado Artefacto");
-
-		estadoArtefactoItem.setOutcome("estadoVista");
-		estadoArtefactoItem.setIcon("icon-vcard");
-		estadoArtefactoItem.setId("sm_estadoVista");
-		cpArtefactoSubmenu.addElement(estadoArtefactoItem);
-
-		//Prioridad Artefacto
-		DefaultMenuItem prioridadArtefactoItem = new DefaultMenuItem("Prioridad Artefacto");
-
-		prioridadArtefactoItem.setOutcome("prioridadVista");
-		prioridadArtefactoItem.setIcon("icon-news");
-		prioridadArtefactoItem.setId("sm_prioridadVista");
-		cpArtefactoSubmenu.addElement(prioridadArtefactoItem);
-
-		//tipo Artefacto
-		DefaultMenuItem tpArtefactoItem = new DefaultMenuItem("Tipo de Artefacto");
-
-		tpArtefactoItem.setOutcome("tipoArtefactoVista");
-		tpArtefactoItem.setIcon("icon-calendar-2");
-		tpArtefactoItem.setId("sm_tipoArtefactoVista");
-		cpArtefactoSubmenu.addElement(tpArtefactoItem);
-
-		cpArtefactoSubmenu.setId("sm_Comportamiento_Artefacto");
-		cpArtefactoSubmenu.setIcon("icon-list-alt");
-
-		menuModel.addElement(cpArtefactoSubmenu);
-
+		//Item Interes
+		DefaultMenuItem interesItem = new DefaultMenuItem("Gestión Interes");
+		interesItem.setOutcome("/XHTML/listaInteres.xhtml");
+		interesItem.setIcon("icon-clipboard");
+		interesItem.setId("icon-stackoverflow");
 		
-	}
+		propiedadesSM.setId("sm_PropArte");
+		propiedadesSM.setIcon("icon-cog-alt");
+		propiedadesSM.addElement(rolItem);
+		propiedadesSM.addElement(prioridadItem);
+		propiedadesSM.addElement(estadoItem);
+		propiedadesSM.addElement(tipoItem);
+		propiedadesSM.addElement(interesItem);
+		menuModel.addElement(propiedadesSM);
 	
+		//Orientacion
+		
+		DefaultSubMenu orientacion = new DefaultSubMenu("Orientación del menu");		
+		
+		DefaultMenuItem derechaItem = new DefaultMenuItem("Derecha");
+		derechaItem.setIcon("icon-align-right");
+		derechaItem.setId("sm_rtl");
+		derechaItem.setOnclick("$('body').addClass('ui-sentinel-rtl');return false;");
+		
+		DefaultMenuItem izquierdaItem = new DefaultMenuItem("Izquierda");
+		izquierdaItem.setIcon("icon-align-left");
+		izquierdaItem.setId("sm_ltr");
+		izquierdaItem.setOnclick("$('body').removeClass('ui-sentinel-rtl');return false;");
+		
+		orientacion.setId("sm_orientation");
+		orientacion.setIcon("icon-align-right");
+		orientacion.addElement(derechaItem);
+		orientacion.addElement(izquierdaItem);
+		menuModel.addElement(orientacion);
+	}
+
 }
